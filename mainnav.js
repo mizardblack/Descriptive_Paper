2910a725ad7f1f91068438f0379111aea80f9c90
Simone <56943425+mizardblack@users.noreply.github.com>
2019-12-16 06:06:58 -0500

    where is omg folder?

---
+++ mainnav.js
@@ -0,0 +1,6 @@
+document.getElementById("navMenu").innerHTML =
+     '<ul>' +
+     '<li><a href="index.html">Home</a></li>' +
+     '<li><a href="paper.html">Descriptive Paper</a></li>' +
+     '<li><a href="more.html">More</a></li>' +
+     '</ul>';
